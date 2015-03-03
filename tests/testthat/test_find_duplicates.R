9739f8f9a87475fc4101ca3e9a9ebbcf843682d7
Clemens Baumbach <clemens.baumbach@helmholtz-muenchen.de>
2015-03-03 14:00:21 +0100

    Add failing test case for find_duplicates.
    We expect an error to be thrown if any variables in COLS or SELECT are
    not in colnames(D).

--- tests/testthat/test_find_duplicates.R
+++ tests/testthat/test_find_duplicates.R
@@ -66,3 +66,16 @@ x y z
     expect_that(find_duplicates(d, c("x", "y")),      equals(d[0L,]))
     expect_that(find_duplicates(d, c("x", "z")),      equals(d[0L,]))
 })
+
+test_that("we throw an error when finding non-existing columns", {
+    d <- read.table(textConnection("\
+x y z
+1 2 3
+4 5 6
+7 8 9
+", "r"), header = TRUE)
+
+    expect_that(find_duplicates(d, "w"),              throws_error("nonexistent variables in COLS"))
+    expect_that(find_duplicates(d, "w", c("y", "z")), throws_error("nonexistent variables in COLS"))
+    expect_that(find_duplicates(d, "x", c("w", "z")), throws_error("nonexistent variables in SELECT"))
+})
