ea5310a2cbb1a9cb49a4e74b7b8843a67f96dfda
Clemens Baumbach <clemens.baumbach@helmholtz-muenchen.de>
2015-03-04 11:06:04 +0100

    Make tests for match_intervals quiet.

--- tests/testthat/test_match_intervals.R
+++ tests/testthat/test_match_intervals.R
@@ -8,7 +8,7 @@ test_that("everything works fine in the vanilla use case", {
     end   <- c(1L,2L,5L)
     id    <- c(1L,2L,3L)
 
-    expect_that(match_intervals(pos, start, end, id), equals(c("1,2", "2", "")))
+    expect_that(match_intervals(pos, start, end, id, quiet = TRUE), equals(c("1,2", "2", "")))
 })
 
 test_that("we get NAs of type 'character' for NA positions", {
@@ -17,7 +17,7 @@ test_that("we get NAs of type 'character' for NA positions", {
     end   <- c(1L,2L,5L)
     id    <- c(1L,2L,3L)
 
-    x <- match_intervals(pos, start, end, id)
+    x <- match_intervals(pos, start, end, id, quiet = TRUE)
 
     expect_that(is.na(x[2]) && typeof(x[2]) == "character", is_true())
 })
@@ -28,28 +28,28 @@ test_that("we ignore missing values in start, end, or id", {
     end   <- c(1L,2L,5L,2L)
     id    <- c(1L,2L,3L,4L)
 
-    x1 <- match_intervals(pos, start, end, id)
+    x1 <- match_intervals(pos, start, end, id, quiet = TRUE)
 
     pos   <- c(1L,2L,3L)
     start <- c(0L,1L,4L,0L)
     end   <- c(1L,NA,5L,2L)             # missing end position
     id    <- c(1L,2L,3L,4L)
 
-    x2 <- match_intervals(pos, start, end, id)
+    x2 <- match_intervals(pos, start, end, id, quiet = TRUE)
 
     pos   <- c(1L,2L,3L)
     start <- c(0L,1L,4L,0L)
     end   <- c(1L,2L,5L,2L)
     id    <- c(1L,NA,3L,4L)             # missing id
 
-    x3 <- match_intervals(pos, start, end, id)
+    x3 <- match_intervals(pos, start, end, id, quiet = TRUE)
 
     pos   <- c(1L,2L,3L)
     start <- c(NA,NA,NA,NA)                # no valid interval
     end   <- c(1L,2L,5L,2L)
     id    <- c(1L,2L,3L,4L)
 
-    x4 <- match_intervals(pos, start, end, id)
+    x4 <- match_intervals(pos, start, end, id, quiet = TRUE)
 
     expect_that(x1, equals(c("1,4", "4", "")))
     expect_that(x2, equals(c("1,4", "4", "")))
