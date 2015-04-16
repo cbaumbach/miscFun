62e6a11ef8e762ef2d1bcdb59cd3ca16e053885a
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-04-16 17:32:44 +0200

    Add test cases for `nsubexp'.

---
+++ tests/testthat/test_nsubexp.R
@@ -0,0 +1,10 @@
+context("Counting parenthesized subexpressions in regular expression patterns")
+
+test_that("nsubexp works", {
+    expect_that(nsubexp(""),                                equals(0L))
+    expect_that(nsubexp(" "),                               equals(0L))
+    expect_that(nsubexp("[()]"),                            equals(0L))
+    expect_that(nsubexp("([()])"),                          equals(1L))
+    expect_that(nsubexp("(()[()])"),                        equals(2L))
+    expect_that(nsubexp("an_(expr)_with_(4)_sub(ex(p)r)s"), equals(4L))
+})
