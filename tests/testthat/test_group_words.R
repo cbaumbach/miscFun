715b1180b63bd74fe34b35158da428f22c9aadbe
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-04-16 18:30:39 +0200

    Add test cases for `group_words'.

---
+++ tests/testthat/test_group_words.R
@@ -0,0 +1,14 @@
+context("Group words in chunks of given length")
+
+test_that("group_words works", {
+    s <- "The quick yellow-orange-brown fox jumps over the 3-year-old, lazy dog."
+
+    ## Split on white space or after hyphens.
+    x <- group_words(s, 10, split = "\\s+|(?<=-)", special = "-$")
+
+    expect_that(length(x), equals(1L))
+    expect_that(all(vapply(x[[1L]], nchar, integer(1L)) <= 10L), is_true())
+    expect_that(!any(grepl("^\\s+", x[[1L]], perl = TRUE)), is_true())
+    expect_that(!any(grepl("\\s+$", x[[1L]], perl = TRUE)), is_true())
+    expect_that(!any(grepl("^[[:punct:]]", x[[1L]])), is_true())
+})
