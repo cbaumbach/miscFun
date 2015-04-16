3d02e6e97f2d77549e72363ae9ea52f296cb5299
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-04-16 18:36:33 +0200

    Add test cases for `make_observer'.

---
+++ tests/testthat/test_make_observer.R
@@ -0,0 +1,16 @@
+context("Create function for keeping track of values")
+
+test_that("make_observer works", {
+    seen <- make_observer()
+
+    expect_that(seen(1), is_false())
+    expect_that(seen(2), is_false())
+    expect_that(seen(0), is_false())
+    expect_that(seen(3), is_false())
+    expect_that(seen(1), is_true())
+    expect_that(seen(1), is_true())
+    expect_that(seen(3), is_true())
+    expect_that(seen(4), is_false())
+    expect_that(seen(show = TRUE), equals(as.character(0:4)))
+    expect_that(seen(NA), throws_error('Argument must be of length 1 and class "character", "numeric", or "integer".'))
+})
