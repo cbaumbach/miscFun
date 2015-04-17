0fcb7e2954e24875896d8b0179694e44da31f93e
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-04-17 10:46:45 +0200

    Add test cases for `noop'.

---
+++ tests/testthat/test_noop.R
@@ -0,0 +1,11 @@
+context("Return nothing")
+
+test_that("noop works", {
+    expect_that(noop(stop()), equals(NULL))
+    expect_that(noop(warning()), equals(NULL))
+    expect_that(noop(exit()), equals(NULL))
+    expect_that(noop(undefined_value), equals(NULL))
+    expect_that(noop(undefined_function()), equals(NULL))
+    expect_that(noop(1, 2, 3), equals(NULL))
+    expect_that(noop(1, 2, 3, stop(), warning(), exit()), equals(NULL))
+})
