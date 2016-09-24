d84633a6f82315430561f5c942c23e0bcc1e6258
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-09-24 22:55:43 +0200

    Refactor test cases for flip function.

--- tests/testthat/test_flip.R
+++ tests/testthat/test_flip.R
@@ -1,17 +1,30 @@
 context("flip")
 
-test_that("only valid functions are flipped", {
-    f <- function(x, y) x + y
+f <- function(x, y, ...) {
+    paste(x, y, ...)
+}
 
-    expect_that((flip(function(x, y) paste0(x, y)))(1,2), equals("21"))
-    expect_that((flip(function(x, y, ...) paste0(x, y, ...)))(1,2,3), equals("213"))
-    expect_that((flip(function(x, y, ..., z) paste0(x, y, z, ...)))(1, 2, 3, z = 4), equals("2143"))
-    expect_that(identical(environment(flip(f)), environment(f)), is_true())
-    expect_that(identical(body(flip(f)), body(f)), is_true())
-    expect_that(flip(function()  1L), throws_error("Function must take at least 2 arguments."))
-    expect_that(flip(function(x) 1L), throws_error("Function must take at least 2 arguments."))
-    expect_that(flip(function(x, ...)    1L), throws_error("Function must not have \"...\" as 1st or 2nd argument."))
-    expect_that(flip(function(..., x)    1L), throws_error("Function must not have \"...\" as 1st or 2nd argument."))
-    expect_that(flip(function(x, ..., y) 1L), throws_error("Function must not have \"...\" as 1st or 2nd argument."))
-    expect_that(flip(function(..., x, y) 1L), throws_error("Function must not have \"...\" as 1st or 2nd argument."))
+test_that("happy path", {
+    expect_identical(flip(f)(1, 2, 3), "2 1 3")
+})
+
+test_that("flip(f) and f have the same environment", {
+    expect_identical(environment(flip(f)), environment(f))
+})
+
+test_that("flip(f) and f have the same body", {
+    expect_identical(body(flip(f)), body(f))
+})
+
+test_that("the function to be flipped must take at least 2 arguments", {
+    error_message <- "Function must take at least 2 arguments."
+    expect_error(flip(function() 1), error_message)
+    expect_error(flip(function(x) 1), error_message)
+})
+
+test_that("the function to be flipped doesn't have \"...\" as 1st or 2nd argument", {
+    error_message <- "Function must not have \"...\" as 1st or 2nd argument."
+    expect_error(flip(function(...) 1), error_message)
+    expect_error(flip(function(x, ...) 1), error_message)
+    expect_error(flip(function(..., x) 1), error_message)
 })
