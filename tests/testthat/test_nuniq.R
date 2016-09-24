8d65ede759b6c649d959aaa01ea2fee116050167
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-09-24 23:14:38 +0200

    Refactor test cases for nuniq function.

--- tests/testthat/test_nuniq.R
+++ tests/testthat/test_nuniq.R
@@ -1,13 +1,27 @@
 context("nuniq")
 
-test_that("nuniq handles missings and factors correctly", {
-    expect_that(nuniq(NA),                                      equals(1L))
-    expect_that(nuniq(c(NA_integer_, NA_real_, NA_character_)), equals(1L))
-    expect_that(nuniq(factor(letters[1:3])),                    equals(3L))
+test_that("happy path", {
+    expect_equal(nuniq(c(1, 2, 1)), 2)
+    expect_equal(nuniq(list(1, 2, 1)), 2)
 })
 
-test_that("nuniq handles non-vector lists correctly", {
-    expect_that(nuniq(list(1, 1)),                                 equals(1L))
-    expect_that(nuniq(list(NA_integer_, NA_real_, NA_character_)), equals(3L))
-    expect_that(nuniq(list(1:2, 1:2)),                             equals(1L))
+test_that("an empyt vector/list contains 0 elements", {
+    expect_equal(nuniq(integer()), 0)
+    expect_equal(nuniq(list()), 0)
+})
+
+test_that("NULL contains 0 elements", {
+    expect_equal(nuniq(NULL), 0)
+})
+
+test_that("NA counts as a unique value", {
+    expect_equal(nuniq(c(1, NA)), 2)
+})
+
+test_that("subtypes of NA count as the same value", {
+    expect_equal(nuniq(list(NA, NA_integer_, NA_real_, NA_character_, NA_complex_)), 5)
+})
+
+test_that("a factor has as many unique elements as levels", {
+    expect_equal(nuniq(factor(1:3)), 3)
 })
