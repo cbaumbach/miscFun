002d3b83ca35468caf09bd04dfae7a7fc51cb4a0
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-09-25 23:46:05 +0200

    Refactor test cases of rename function.

--- tests/testthat/test_rename.R
+++ tests/testthat/test_rename.R
@@ -1,19 +1,42 @@
 context("rename")
 
-test_that("rename works", {
-    d <- expand.grid(x = 1:2, y = c(TRUE, FALSE))
+test_that("happy path", {
+    expect_equal(rename(c(A = 1, B = 2), c(A = "a", B = "b")), c("a", "b"))
+})
 
-    expect_that(rename(d), throws_error("argument \"old2new\" is missing, with no default"))
-    expect_that(rename(d, c("a", "b", "c")), gives_warning("`names' attribute of `old2new' is NULL."))
-    expect_that(rename(d, c("a", "b", "c"), warn = FALSE), equals(names(d)))
-    expect_that(rename(d, c(x = "a", y = "b")), equals(c("a", "b")))
-    expect_that(rename(d, c(y = "b", x = "a")), equals(c("a", "b")))
-    expect_that(rename(d, c(foo = "oof", y = "z", bar = "rab")), gives_warning("The following name tags from `old2new' don't match any element of `names\\(x\\)': \"foo\", \"bar\""))
-    expect_that(rename(d, c(foo = "oof", y = "z", bar = "rab"), warn = FALSE), equals(c("x", "z")))
-    expect_that(rename(d, c(x = "y", y = "x")), equals(c("y", "x")))
-    expect_that(rename(d, c(x = "a", "b")), equals(c("a", "y")))
-    expect_that(rename(d, c("b", x = "a")), equals(c("a", "y")))
-    expect_that(rename(cbind(d, d), c(x = "a", y = "b")), gives_warning("Duplicates after renaming: \"a\", \"b\""))
-    expect_that(rename(cbind(d, d), c(x = "a", y = "b"), warn = FALSE), equals(rep_len(c("a", "b"), 4L)))
-    expect_that(rename(d, c(x = "y")), gives_warning("Duplicates after renaming: \"y\""))
+test_that("we get a warning if old2new has no names attribute", {
+    expect_warning(rename(1, 2), "`names' attribute of `old2new' is NULL.")
+})
+
+test_that("if old2new has no `names' attribute, `rename' is a no-op", {
+    expect_equal(rename(c(A = 1, B = 2), 1, warn = FALSE), c("A", "B"))
+})
+
+test_that("the order of names in old2new can be different from the order of names of x", {
+    expect_equal(rename(c(A = 1, B = 2), c(B = "b", A = "a")), c("a", "b"))
+})
+
+test_that("we get a warning for names of old2nem that don't match any names in x", {
+    expect_warning(rename(c(A = 1), c(B = "b")), "The following name tags from `old2new' don't match any element of `names\\(x\\)': \"B\"")
+})
+
+test_that("old2new may contain names not matching any names in x", {
+    expect_equal(rename(c(A = 1, B = 2), c(A = "a", C = "c"), warn = FALSE), c("a", "B"))
+})
+
+test_that("it is possible to swap the names of elements of x", {
+    expect_equal(rename(c(A = 1, B = 2), c(A = "B", B = "A")), c("B", "A"))
+})
+
+test_that("old2new may have a mixture of named and unnamed elements", {
+    expect_equal(rename(c(A = 1), c("X", A = "a")), "a")
+})
+
+test_that("we get a warning if renaming would result in duplicate names", {
+    expect_warning(rename(c(A = 1, B = 2), c(A = "C", B = "C")), "Duplicates after renaming: \"C\"")
+    expect_warning(rename(c(A = 1, B = 2), c(A = "B")), "Duplicates after renaming: \"B\"")
+})
+
+test_that("it is possible to give multiple elements of x the same name", {
+    expect_equal(rename(c(A = 1, B = 2), c(A = "x", B = "x"), warn = FALSE), c("x", "x"))
 })
