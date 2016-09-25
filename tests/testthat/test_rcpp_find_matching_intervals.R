3a5d23f3cc1b5099843cf0af7819e4f4775fe96d
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-09-25 16:47:18 +0200

    Refactor rcpp_find_matching_intervals test cases.

--- tests/testthat/test_rcpp_find_matching_intervals.R
+++ tests/testthat/test_rcpp_find_matching_intervals.R
@@ -2,8 +2,10 @@ context("rcpp_find_matching_intervals")
 
 empty <- list(integer(), integer())
 
+f <- rcpp_find_matching_intervals
+
 test_that("the components of the returned list are called 'position' and 'interval'", {
-    actual <- names(rcpp_find_matching_intervals(0, 0, 0))
+    actual <- names(f(0L, 0L, 0L))
     expected <- c("position", "interval")
     expect_equal(actual, expected)
 })
@@ -12,32 +14,37 @@ test_that("nested intervals are handled correctly", {
     start <- c(-3L, -1L)
     end   <- c(+3L, +1L)
     positions <- c(-5L, -2L, 0L, 2L, 5L)
-    actual <- rcpp_find_matching_intervals(positions, start, end)
+    actual <- f(positions, start, end)
     expected <- list(c(2L, 3L, 3L, 4L),
                      c(1L, 1L, 2L, 1L))
     expect_equivalent(actual, expected)
 })
 
 test_that("intervals are closed", {
-    expect_equivalent(rcpp_find_matching_intervals(0L, 0L, 0L), list(1L, 1L))
+    expect_equivalent(f(0L, 0L, 0L), list(1L, 1L))
 })
 
 test_that("NA's belong to no interval", {
-    expect_equivalent(rcpp_find_matching_intervals(NA, 0L, 0L), empty)
+    expect_equivalent(f(NA, 0L, 0L), empty)
 })
 
 test_that("nothing belongs to [NA, NA]", {
-    expect_equivalent(rcpp_find_matching_intervals(0L, NA, NA), empty)
+    expect_equivalent(f(0L, NA, NA), empty)
 })
 
 test_that("nothing belongs to [NA, *]", {
-    expect_equivalent(rcpp_find_matching_intervals(0L, NA, 0L), empty)
+    expect_equivalent(f(0L, NA, 0L), empty)
 })
 
 test_that("nothing belongs to [*, NA]", {
-    expect_equivalent(rcpp_find_matching_intervals(0L, 0L, NA), empty)
+    expect_equivalent(f(0L, 0L, NA), empty)
 })
 
 test_that("nothing falls into an empty interval", {
-    expect_equivalent(rcpp_find_matching_intervals(0L, 1L, -1L), empty)
+    expect_equivalent(f(0L, 1L, -1L), empty)
+})
+
+test_that("start positions don't have to be sorted", {
+    actual <- f(0L, c(99L, 0L), c(99L, 0L))
+    expect_equal(actual$interval, 2)
 })
