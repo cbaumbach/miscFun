0083a04bb2e53536480f7f734ee643caafb4fdaf
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-09-25 15:36:52 +0200

    Refactor.

--- src/rcpp_find_matching_intervals.cpp
+++ src/rcpp_find_matching_intervals.cpp
@@ -1,20 +1,16 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-static bool is_na_interval(int start, int end)
-{
+static bool is_na_interval(int start, int end) {
     return (start == NA_INTEGER) || (end == NA_INTEGER);
 }
 
-static bool belongs_to_interval(int pos, int start, int end)
-{
+static bool belongs_to_closed_interval(int pos, int start, int end) {
     return (start <= pos) && (pos <= end);
 }
 
-static int get_last_index(int pos, IntegerVector& start)
-{
-    return std::upper_bound(start.begin(), start.end(), pos)
-                    - start.begin();
+static int find_index_of_first_element_greater_than(int value, IntegerVector& xs) {
+    return std::upper_bound(xs.begin(), xs.end(), value) - xs.begin();
 }
 
 //' Find intervals containing a set of points
@@ -37,19 +33,16 @@ static int get_last_index(int pos, IntegerVector& start)
 //'
 //' @export
 // [[Rcpp::export]]
-List rcpp_find_matching_intervals(IntegerVector pos,
-        IntegerVector start, IntegerVector end)
-{
+List rcpp_find_matching_intervals(IntegerVector pos, IntegerVector start, IntegerVector end) {
     std::vector<int> index_position;
     std::vector<int> index_interval;
-
     for (int i = 0; i < pos.size(); i++) {
         int position = pos[i];
-        int last_index = get_last_index(position, start);
+        int last_index = find_index_of_first_element_greater_than(position, start);
         for (int j = 0; j < last_index; j++) {
             if (is_na_interval(start[i], end[i]))
                 continue;
-            if (belongs_to_interval(position, start[j], end[j])) {
+            if (belongs_to_closed_interval(position, start[j], end[j])) {
                 index_position.push_back(i + 1);
                 index_interval.push_back(j + 1);
             }
