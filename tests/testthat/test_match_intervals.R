d08a9aff5762bf90acda076712abe3a80235479c
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-10-13 20:22:46 +0200

    Refactor test cases for match_intervals function.

--- tests/testthat/test_match_intervals.R
+++ tests/testthat/test_match_intervals.R
@@ -1,56 +1,27 @@
 context("match_intervals")
 
-test_that("everything works fine in the vanilla use case", {
-    pos   <- c(1L,2L,3L)
-    start <- c(0L,1L,4L)
-    end   <- c(1L,2L,5L)
-    id    <- c(1L,2L,3L)
+f <- match_intervals
 
-    expect_that(match_intervals(pos, start, end, id, quiet = TRUE), equals(c("1,2", "2", "")))
+test_that("happy path", {
+    expect_equal(f(c(1, 2, 999), c(0, 1.5), c(3, 3), c("A", "B")), c("A", "A,B", ""))
 })
 
-test_that("we get NAs of type 'character' for NA positions", {
-    pos   <- c(1L,NA,3L)
-    start <- c(0L,1L,4L)
-    end   <- c(1L,2L,5L)
-    id    <- c(1L,2L,3L)
-
-    x <- match_intervals(pos, start, end, id, quiet = TRUE)
-
-    expect_that(is.na(x[2]) && typeof(x[2]) == "character", is_true())
+test_that("NA positions result in NAs", {
+    expect_equal(f(NA, 1, 2, "A"), NA_character_)
 })
 
-test_that("we ignore missing values in start, end, or id", {
-    pos   <- c(1L,2L,3L)
-    start <- c(0L,NA,4L,0L)             # missing start position
-    end   <- c(1L,2L,5L,2L)
-    id    <- c(1L,2L,3L,4L)
+test_that("NAs in 'start' are ignored", {
+    expect_equal(f(1, c(0, NA), c(2, 3), c("A", "B")), "A")
+})
 
-    x1 <- match_intervals(pos, start, end, id, quiet = TRUE)
+test_that("NAs in 'end' are ignored", {
+    expect_equal(f(1, c(0, 1), c(1, NA), c("A", "B")), "A")
+})
 
-    pos   <- c(1L,2L,3L)
-    start <- c(0L,1L,4L,0L)
-    end   <- c(1L,NA,5L,2L)             # missing end position
-    id    <- c(1L,2L,3L,4L)
+test_that("NAs in 'id' are ignored", {
+    expect_equal(f(1, 0, 2, NA), "")
+})
 
-    x2 <- match_intervals(pos, start, end, id, quiet = TRUE)
-
-    pos   <- c(1L,2L,3L)
-    start <- c(0L,1L,4L,0L)
-    end   <- c(1L,2L,5L,2L)
-    id    <- c(1L,NA,3L,4L)             # missing id
-
-    x3 <- match_intervals(pos, start, end, id, quiet = TRUE)
-
-    pos   <- c(1L,2L,3L)
-    start <- c(NA,NA,NA,NA)                # no valid interval
-    end   <- c(1L,2L,5L,2L)
-    id    <- c(1L,2L,3L,4L)
-
-    x4 <- match_intervals(pos, start, end, id, quiet = TRUE)
-
-    expect_that(x1, equals(c("1,4", "4", "")))
-    expect_that(x2, equals(c("1,4", "4", "")))
-    expect_that(x3, equals(c("1,4", "4", "")))
-    expect_that(x4, equals(c("","","")))
+test_that("positions that don't fall into any interval result in an empty string", {
+    expect_equal(f(999, 0, 1, "A"), "")
 })
