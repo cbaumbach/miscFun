c4f337e957f71ddf32f07884eedb549c078f4872
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-10-13 22:59:42 +0200

    Refactor rev_map test cases.

--- tests/testthat/test_rev_map.R
+++ tests/testthat/test_rev_map.R
@@ -1,52 +1,34 @@
 context("rev_map")
 
-test_that("simple cases work as expected", {
-    map <- list(a = c("A","C"), b = c("B","C"))
-
-    expect_that(rev_map(map), equals(
-        list(A = "a", B = "b", C = c("a","b"))))
+test_that("happy path", {
+    x <- rev_map(list(a = c("A", "C"), b = c("B", "C")))
+    expect_equal(x$A, "a")
+    expect_equal(x$B, "b")
+    expect_equal(x$C, c("a", "b"))
 })
 
-test_that("\"NA\" tags are turned into \"NA\" strings", {
-    map <- list(a = c("A","C"), "NA" = c("A","C"), b = c("B","C"))
-
-    ## NA's come last because of sorting.
-    expect_that(rev_map(map), equals(
-        list(A = c("a","NA"), B = "b", C = c("a","b","NA"))))
+test_that("NA tags are turned into NA strings", {
+    x <- rev_map(list("NA" = "A"))
+    expect_equal(x$A, "NA")
 })
 
-test_that("missing values are dropped", {
-    map <- list(a = c(NA,"A","C"), b = c("B","C",NA))
-
-    expect_that(rev_map(map), equals(
-        list(A = "a", B = "b", C = c("a","b"))))
+test_that("NA values are dropped", {
+    x <- rev_map(list(a = c(NA, "A")))
+    expect_equal(names(x), "A")
 })
 
-test_that("the reverse of the reverse of a map without missing values is the map itself", {
-    map <- list(a = c("A","C"), b = c("B","C"))
-    rrmap <- rev_map(rev_map(map))
+test_that("we get an empty map if all values are NA", {
+    x <- rev_map(list(a = NA))
+    expect_true(is.list(x))
+    expect_equal(length(x), 0)
+})
 
-    ## Sort elements.
-    map   <- lapply(map, sort)
-    rrmap <- lapply(rrmap, sort)
-
-    expect_that(rrmap, equals(map))
+test_that("if there are no NA values the reverse of the reverse is the map itself modulo the order of elements", {
+    map <- list(a = c("A", "B"), b = c("B", "C"))
+    expect_equal(rev_map(rev_map(map)), map)
 })
 
 test_that("reversing a map twice removes keys without values", {
-    map <- list(a = c("A","C"), b = c("B","C"), c = NULL, d = character(0L))
-    map2 <- list(a = c("A","C"), b = c("B","C"))
-    rrmap <- rev_map(rev_map(map))
-
-    sort_elements <- function(x) {
-        if (is.null(x))
-            x
-        else
-            sort(x)
-    }
-    map   <- lapply(map, sort_elements)
-    map2  <- lapply(map2, sort_elements)
-    rrmap <- lapply(rrmap, sort_elements)
-
-    expect_that(rrmap, equals(map2))
+    x <- rev_map(rev_map(list(a = "A", b = NULL, c = integer())))
+    expect_equal(names(x), "a")
 })
